cc43dddf9ed5fc266ada151b9db8fa797e39f087
sarmanchisarajji <sarmanchysara@gmail.com>
2023-10-24 12:17:45 +0800

    first template for admin and mahasisw

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/mhs', function () {
+    return view('mahasiswa.layout.app');
+});
+
+Route::get('/admin', function () {
+    return view('admin.layout.app');
 });
